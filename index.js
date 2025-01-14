9f74f0d71e1645304a3758dfa179fac4742158f2
Maxim Izrailev <max.izrailev04@mail.ru>
2025-01-14 14:17:11 +0300

    add ckecking fn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-console.log('Hello, Hexlet!');
-console.log('Check on working');
+console.log("Hello, Hexlet!");
+console.log("Check on working");
 import _ from "lodash";
 console.log(_.intersection([2, 1], [2, 3]));
-console.log(_.last(['one', 'two', 'three']));
+console.log(_.last(["one", "two", "three"]));
+const fn = (a, b) => {
+  const c = a + b;
+  return c;
+};
+console.log(fn(43, 9));
